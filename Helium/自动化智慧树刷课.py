302be8f767d305f69bcbd5d678a457fb0ac6b817
luobo <fuyu16032001@gmail.com>
2020-08-24 19:01:13 +0800

    add za

---
+++ Helium/自动化智慧树刷课.py
@@ -0,0 +1,7 @@
+from helium import *
+
+
+start_chrome("https://passport.zhihuishu.com/login?service=https://onlineservice.zhihuishu.com/login/gologin")
+write("18846936702",TextField('请输入手机号'))
+write("zxc4525577",TextField('请输入密码'))
+press(ENTER)
