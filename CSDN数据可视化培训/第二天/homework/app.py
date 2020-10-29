e58d536a837427b4c22623e1af76d9a84f683c02
luoboovo <fuyu16032001@gmail.com>
2020-10-29 14:09:50 +0800

    add aaaa

--- CSDN数据可视化培训/第二天/homework/app.py
+++ CSDN数据可视化培训/第二天/homework/app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/html')
+@app.route('/')
 def two_day():
-    title = '第二天的作业'
+    title = '全国计程车统计'
     return render_template('index.html', title=title)
 
 if __name__ == '__main__':
