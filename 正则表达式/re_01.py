348790df783588258f0119e4d6f78ae76d3fdd22
luoboovo <fuyu16032001@gmail.com>
2020-12-07 14:29:49 +0800

    add yidadui

--- 正则表达式/re_01.py
+++ 正则表达式/re_01.py
@@ -1,10 +1,12 @@
 import re
-
-content = "Hello 123 4567 World_This is a Regex Demo"
+import collections
+content = "Hello 123 4567 World_This i128763876`12s a Regex Demo"
 
 print(len(content))
 
 result = re.match('^H.*?(\d+)\s(\d+).*mo$', content)
+
+
 
 print(result)
 # print(result.group(1), end = '')
